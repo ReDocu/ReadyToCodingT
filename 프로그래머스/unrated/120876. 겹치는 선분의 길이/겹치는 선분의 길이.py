609a67fe32af85f64dbf2a7dfdab86c12e421427
ReDocu <152945033+ReDocu@users.noreply.github.com>
2024-01-09 00:24:28 +0900

    [level unrated] Title: 겹치는 선분의 길이, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/120876. 겹치는 선분의 길이/겹치는 선분의 길이.py
+++ 프로그래머스/unrated/120876. 겹치는 선분의 길이/겹치는 선분의 길이.py
@@ -1,22 +1,5 @@
-def solution(lines):    
-
-    checkline = sorted(lines, key=lambda x:x[0])
-    count = set()
-
-    for x in range(checkline[0][0], checkline[0][1]):
-        for y in range(checkline[1][0], checkline[1][1]):
-            if x  == y:
-                count.add(x)
-
-    for x in range(checkline[1][0], checkline[1][1]):
-        for y in range(checkline[2][0], checkline[2][1]):
-            if x  == y:
-                count.add(x)
-
-    for x in range(checkline[2][0], checkline[2][1]):
-        for y in range(checkline[0][0], checkline[0][1]):
-            if x  == y:
-                count.add(x)
-
-
-    return len(count)
+def solution(lines):
+    
+    lines
+    
+    return answer
